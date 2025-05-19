54da6c34ea46e3b2871a1cc1c42d62d9a283dc1d
patnorris <patrick@DESKTOP-TQ4V8LQ>
2025-05-19 13:41:18 +0200

    Add development stage to scripts

--- src/Challenger/scripts/register-llms.sh
+++ src/Challenger/scripts/register-llms.sh
@@ -17,7 +17,7 @@ while [ $# -gt 0 ]; do
     case "$1" in
         --network)
             shift
-            if [ "$1" = "local" ] || [ "$1" = "ic" ] || [ "$1" = "testing" ]; then
+            if [ "$1" = "local" ] || [ "$1" = "ic" ] || [ "$1" = "testing" ] || [ "$1" = "development" ]; then
                 NETWORK_TYPE=$1
                 if [ "$NETWORK_TYPE" = "ic" ]; then
                     CANISTER_ID_LLM_0='lmehs-taaaa-aaaaj-azzrq-cai'
@@ -27,7 +27,7 @@ while [ $# -gt 0 ]; do
                     CANISTER_ID_LLM_1='cz7tl-cqaaa-aaaaj-az77a-cai' 
                 fi
             else
-                echo "Invalid network type: $1. Use 'local' or 'ic' or 'testing."
+                echo "Invalid network type: $1. Use 'local', 'development' or 'ic' or 'testing."
                 exit 1
             fi
             shift
@@ -41,7 +41,7 @@ while [ $# -gt 0 ]; do
 done
 
 echo "Using network type: $NETWORK_TYPE"
-if [ "$NETWORK_TYPE" = "ic" ] || [ "$NETWORK_TYPE" = "testing" ]; then
+if [ "$NETWORK_TYPE" = "ic" ] || [ "$NETWORK_TYPE" = "testing" ] || [ "$NETWORK_TYPE" = "development" ]; then
     NUM_LLMS_DEPLOYED=2
 fi
 
